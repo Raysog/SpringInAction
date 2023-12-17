aa24aef9fc398a38404a9f070e89b0968289f9d4
raysog <w.fedorov.ivan@gmail.com>
2023-12-18 00:21:32 +0300

    -add demo database

---
+++ src/main/java/com/example/springinaction/repetit/dao/TaskReader.java
@@ -0,0 +1,12 @@
+package com.example.springinaction.repetit.dao;
+
+import com.example.springinaction.repetit.testTask.Task;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TaskReader {
+    public Optional<Task> findById(int id) {
+        List<Task>
+    }
+}
